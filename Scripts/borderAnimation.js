e11710ea55050ce579655051387120ec5fd3746a
Ruben Hernandez <rubenhnt@gmail.com>
2021-06-01 15:21:11 -0500

    first commit

---
+++ Scripts/borderAnimation.js
@@ -0,0 +1,27 @@
+﻿var borderLine = document.querySelector("#testBorder");
+
+function setup() {
+    borderLine.addEventListener("load", setInitialClass, false);
+
+    borderLine.addEventListener("transitionend", loopTransition, false);
+    borderLine.addEventListener("webkitTransitionEnd", loopTransition, false);
+    borderLine.addEventListener("mozTransitionEnd", loopTransition, false);
+    borderLine.addEventListener("msTransitionEnd", loopTransition, false);
+    borderLine.addEventListener("oTransitionEnd", loopTransition, false);
+}
+
+setup();
+
+function setInitialClass(e) {
+    borderLine.className = "draw";
+}
+
+function loopTransition(e) {
+    if (e.propertyName == "opacity") {
+        if (borderLine.className == "draw") {
+            borderLine.className = "borderDraw";
+        } else {
+            borderLine.className = "draw";
+        }
+    }
+}
